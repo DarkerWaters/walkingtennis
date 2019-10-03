b1d49a46e5c77c58e80b5fbc709a65e2a9249de1
Douglas Brain <butterbrain77@gmail.com>
2019-10-03 17:50:46 +0100

    made a nicer login screen

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -3,40 +3,88 @@ function signinFirebase() {
     // https://firebase.google.com/docs/auth/web/firebaseui
     var uiConfig = {
         callbacks: {
-            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-                // User successfully signed in.
+            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+                // signed in successfully, hide the box we used to select the type
+                document.getElementById('firebase_login_container').style.display = "none";
+                // and we can get the data here
+                /*
+                var user = authResult.user;
+                var credential = authResult.credential;
+                var isNewUser = authResult.additionalUserInfo.isNewUser;
+                var providerId = authResult.additionalUserInfo.providerId;
+                var operationType = authResult.operationType;
+                // Do something with the returned AuthResult.
+                */
                 // Return type determines whether we continue the redirect automatically
                 // or whether we leave that to developer to handle.
                 return true;
             },
-            uiShown: function() {
+            signInFailure: function (error) {
+                // Some unrecoverable error occurred during sign-in.
+                document.getElementById('firebase_login_container').style.display = "none";
+                // Return a promise when error handling is completed and FirebaseUI
+                // will reset, clearing any UI. This commonly occurs for error code
+                // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+                // occurs. Check below for more details on this.
+                return handleUIError(error);
+            },
+            uiShown: function () {
                 // The widget is rendered, hide the loader button
                 var signIn = document.getElementById('firebaseSignIn');
-                if (signIn) signIn.style.display = 'none';
+                if (signIn) {
+                    signIn.style.display = 'none';
+                }
             }
         },
+        credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
+        // Query parameter name for mode.
+        queryParameterForWidgetMode: 'mode',
+        // Query parameter name for sign in success url.
+        queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
         signInSuccessUrl: '#',
         signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
             firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            /*,firebase.auth.TwitterAuthProvider.PROVIDER_ID
+            {
+                provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                // Whether the display name should be displayed in the Sign Up page.
+                requireDisplayName: true
+            }*/
+            /*,{
+                provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                // Invisible reCAPTCHA with image challenge and bottom left badge.
+                recaptchaParameters: {
+                    type: 'image',
+                    size: 'invisible',
+                    badge: 'bottomleft'
+                }
+            }*/
             //,firebase.auth.FacebookAuthProvider.PROVIDER_ID
-            //,firebase.auth.TwitterAuthProvider.PROVIDER_ID
             //,firebase.auth.GithubAuthProvider.PROVIDER_ID
             ,firebase.auth.EmailAuthProvider.PROVIDER_ID
-            //,firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ],
+        // tosUrl and privacyPolicyUrl accept either url string or a callback
+        // function.
         // Terms of service url.
         tosUrl: 'https://www.walkingtennis.org/adminterms.html',
         // Privacy policy url.
         privacyPolicyUrl: 'https://www.walkingtennis.org/adminprivacy.html'
     };
 
+    // show the container to login with
+    document.getElementById('firebase_login_container').style.display = null;
+
     // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);	
+    if(firebaseui.auth.AuthUI.getInstance()) {
+        const ui = firebaseui.auth.AuthUI.getInstance();
+        ui.start('#firebase_login_container', uiConfig);
+    } else {
+        const ui = new firebaseui.auth.AuthUI(firebase.auth());
+        ui.start('#firebase_login_container', uiConfig);	
+    }
 };
 
 function getFirebaseUser() {
